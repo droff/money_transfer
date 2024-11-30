a36b93381fb3cab2c814732c73662668c1f3e5a5
Andrei <adroff@gmail.com>
2024-11-29 20:37:18 -0500

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'me' => 'api/v1/accounts#me'
   post 'create_account' => 'api/v1/accounts#create'
-  post '/transfer' => 'api/v1/accounts#transfer'
-  post '/deposit' => 'api/v1/accounts#deposit'
-  post '/withdraw' => 'api/v1/accounts#withdraw'
+  post 'transfer' => 'api/v1/accounts#transfer'
+  post 'deposit' => 'api/v1/accounts#deposit'
+  post 'withdraw' => 'api/v1/accounts#withdraw'
 end
